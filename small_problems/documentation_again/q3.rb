f366ded2eb02ffafdee47ef4810266545f18ac50
Aaron Tan <aarontanxa@gmail.com>
2023-03-12 14:35:59 +0100

    Add exercise 3 answer

--- small_problems/documentation_again/q3.rb
+++ small_problems/documentation_again/q3.rb
@@ -1,1 +1,9 @@
-ÿþ
+# Consider the following method and a call to that method:
+
+def my_method(a, b = 2, c = 3, d)
+  p [a, b, c, d]
+end
+
+my_method(4, 5, 6)
+# => [4, 5, 3, 6]
+# Use the ruby documentation to determine what this code will print.
